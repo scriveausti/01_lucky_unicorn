f4f2448cf19ff642204bb04f63cd685e1e4794ad
scriveausti <scrive.austi@awatapu.school.nz>
2021-06-24 09:59:03 +1200

    test

--- new.py
+++ new.py
@@ -1,2 +1,13 @@
-for i in range(0, 1001):
-    print("number ",i)
+def nextline():
+    next_line = False
+    while next_line == False :
+        next = input("go to next line?(yes or no)")
+        next = next.lower().strip()
+        if next == "yes":
+            next_line = True
+        else:
+            next_line = False
+
+nextline()
+print("next line")
+
