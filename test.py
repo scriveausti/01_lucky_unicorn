087b50a6ae3f110bd373c49378180e98c1402ca1
scriveausti <scrive.austi@awatapu.school.nz>
2021-06-30 14:27:02 +1200

    test

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
+# welcomes user
+input("welcome")
+
+
+# asks if they have played before
 answer = False
 while answer == False:
-    new = input("have you played befor?")
+    new = input("have you played before?")
     if new == "yes":
         answer = True
-        break
     elif new == "no":
         print("instruction")
         break
     else:
         print("pleas inter yes or no")
+
+print("test")
