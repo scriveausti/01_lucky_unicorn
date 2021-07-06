578fdaeccd5c47a5a212abf35a565ce5fdf344e4
scriveausti <scrive.austi@awatapu.school.nz>
2021-07-06 15:09:07 +1200

    test

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 def gen():
-    import random
-    n = random.randint(0, 10000000000)
+    n = .randint(0, 10000000000)
     print("{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}" .format(n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, ))
 
 
