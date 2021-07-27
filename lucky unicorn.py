0ac3df8cefcbada6d7ffeb04865d3f642da50c93
scriveausti <scrive.austi@awatapu.school.nz>
2021-07-27 13:29:11 +1200

    yes no check and instruction

--- lucky unicorn.py
+++ lucky unicorn.py
@@ -1,9 +1,20 @@
-#setting variables
+# setting variables
 no = ["no", "n"]
 yes = ["yes", "y"]
 
 # welcomes user
-input("Welcome to lucky unicorn")
+print("Welcome to lucky unicorn")
+input("press enter to continue")
+
+# instructions
+def instructions():
+    print("how to play ")
+    print("")
+    print(" enter the amount of money you would like to gamble ")
+    print("")
+    print("")
+    print("")
+    print("")
 
 # asks if they have played before
 answer = False
@@ -13,16 +24,15 @@ while True:
         print("plays game")
         break
     elif new in no:
-        print("instruction")
+        instructions()
         break
     else:
         print(" <error> please enter yes or no")
 
 # asks how much money they would like to spend
 while True:
-    try:
-        spend = int(input("how much money do you like like to spend between $0 and $10?"))
-        if spend <= 10 or spend > 0 :
-            break
-    except:
+    spend = int(input("how much money do you like like to spend between $0 and $10?"))
+    if 10 >= spend > 0:
+        break
+    else:
         print(" <error> please enter a number between 0 and 10")
