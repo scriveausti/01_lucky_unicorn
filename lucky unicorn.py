ae4d69a401d7722d0a525cf6514537dfe6c940c7
scriveausti <scrive.austi@awatapu.school.nz>
2021-08-02 13:27:24 +1200

    yes no check and instruction

--- lucky unicorn.py
+++ lucky unicorn.py
@@ -8,12 +8,13 @@ input("press enter to continue")
 
 # instructions
 def instructions():
-    print("how to play ")
+    print("")
+    print(" -=-  how to play -=-  ")
     print("")
     print(" enter the amount of money you would like to gamble ")
-    print("")
-    print("")
-    print("")
+    print(" then it will generat a token that will be a zebra, horse, donkey or unicorn")
+    print(" if you get a unicorn you win $5. if you get a zebra or horse you get 50c back")
+    print(" if you get a donkey you don't get any money back")
     print("")
 
 # asks if they have played before
@@ -21,7 +22,6 @@ answer = False
 while True:
     new = input("have you played before?").strip().lower()
     if new in yes:
-        print("plays game")
         break
     elif new in no:
         instructions()
@@ -31,8 +31,11 @@ while True:
 
 # asks how much money they would like to spend
 while True:
-    spend = int(input("how much money do you like like to spend between $0 and $10?"))
-    if 10 >= spend > 0:
-        break
-    else:
-        print(" <error> please enter a number between 0 and 10")
+    try:
+        spend = int(input("how much money do you like like to spend between $0 and $10?"))
+        if 10 >= spend > 0:
+            break
+        else:
+            print(" <error> please enter a whole number between 0 and 10")
+    except:
+        print(" <error> please enter a whole number between 0 and 10")
