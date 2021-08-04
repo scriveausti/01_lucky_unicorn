4b582f9075c94d12d4c1ce1ec3cb1d64e6747b71
scriveausti <scrive.austi@awatapu.school.nz>
2021-08-04 19:34:36 +1200

    rock paper scissors

--- lucky unicorn.py
+++ lucky unicorn.py
@@ -12,7 +12,7 @@ def instructions():
     print(" -=-  how to play -=-  ")
     print("")
     print(" enter the amount of money you would like to gamble ")
-    print(" then it will generat a token that will be a zebra, horse, donkey or unicorn")
+    print(" then it will generate a token that will be a zebra, horse, donkey or unicorn")
     print(" if you get a unicorn you win $5. if you get a zebra or horse you get 50c back")
     print(" if you get a donkey you don't get any money back")
     print("")
@@ -39,3 +39,18 @@ while True:
             print(" <error> please enter a whole number between 0 and 10")
     except:
         print(" <error> please enter a whole number between 0 and 10")
+
+while True:
+    import random
+    ran_number = random.randint(1,8)
+    if ran_number == 1 :
+        token = "unicorn"
+
+    elif ran_number == 2 or ran_number == 3 :
+        token = "horse"
+
+    elif ran_number == 4 or ran_number ==5 :
+        token = "zebra"
+
+    else:
+        token = "donkey"
