e1516cb244f9d628439ecf04efce2b20ec66025a
scriveausti <scrive.austi@awatapu.school.nz>
2021-08-10 13:27:53 +1200

    final

---
+++ rounds v.2.py
@@ -0,0 +1,14 @@
+while True:
+    if token_gen.balance < 1:
+        print("you ran out of money")
+        break
+    else:
+        play_again = input("do you want to continue playing")
+        if play_again in yes:
+            token_gen()
+            print("you got {}".format(token_gen.token))
+            print("your balance is now ${}".format(token_gen.balance))
+        elif play_again in no:
+            break
+        else:
+            print(" <error> please enter yes or no")
